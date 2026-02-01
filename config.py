6a4d22b7aaaab2b9b0de8bed49ab570af764b648
Valdo Joao <valdojoao90@gmail.com>
2026-02-01 21:10:37 +0100

    input data and default config params

---
+++ config.py
@@ -0,0 +1,30 @@
+
+
+
+METADATA = {
+              "input_file":     "./data_input/user_sample.csv",
+              "output_file":    "./data_output/",
+              "input_headers": [
+                                  "user_id",
+                                  "manager_id",
+                                  "name",
+                                  "email_address",
+                                  "start_date",
+                                  "last_login"
+                                ],
+              "output_headers": [
+                                  "manager_id",
+                                  "email_address",
+                                  "start_date",
+                                  "last_login",
+                                  "user_id"
+                                ],
+              "transformations":[
+                                  { "type": "UUIDMapper", "column": "user_id" },
+                                  { "type": "InfoRedactor", "column": "email_address" },
+                                  { "type": "DateFormatter", "column": "last_login" }
+              ],
+              "backend": "builtin"
+}
+
+
